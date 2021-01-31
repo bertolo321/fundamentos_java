4cdb4554f5c863ef802d8ebfa131698d6c0ff745
bertolo321 <andre_bertolo@outlook.com>
2021-01-31 12:01:04 -0300

    Exemplo de casting explicito

---
+++ src/variaveis/Casting.java
@@ -0,0 +1,21 @@
+package variaveis;
+
+public class Casting {
+	public static void main(String[] args) {
+	//alterar tipo de dados
+	
+	//long x = 10;
+	
+	//double x = 35;
+	
+	//float x = 40.0F;
+	
+	//float x = (float)40.0;
+	
+	//short x = (short)100000; erro
+	
+	//byte x = (byte)130; erro
+		
+	//System.out.println(x);
+	}	
+}
