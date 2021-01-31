b3ae666ee292119a3d72f040680e7492e7625bd7
bertolo321 <andre_bertolo@outlook.com>
2021-01-31 00:39:04 -0300

    Exemplos de var

---
+++ src/variaveis/VariaveisTipoVar.java
@@ -0,0 +1,15 @@
+package variaveis;
+
+public class VariaveisTipoVar {
+
+	public static void main(String[] args) {
+		
+		int x = 10;
+		var x2 = 20; //não pode trocar o tipo depois da inicialização
+		boolean y = false;
+		var y2 = true;
+		
+		//var z; errado tem que inicializar a variável
+	}
+
+}
