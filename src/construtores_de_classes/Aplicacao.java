760dc7e568ae627eb77b8a3760bbba4bcd1c08ed
bertolo321 <andre_bertolo@outlook.com>
2021-02-02 01:16:34 -0300

    Exemplo de construtores de classes.

---
+++ src/construtores_de_classes/Aplicacao.java
@@ -0,0 +1,21 @@
+package construtores_de_classes;
+
+public class Aplicacao {
+
+	public static void main(String[] args) {
+		Retangulo r1 = new Retangulo();
+	    double area1 = r1.calcularArea();
+	    
+	    System.out.println(area1);
+	    
+	    Retangulo r2 = new Retangulo(5,7);
+	    double area2 = r2.calcularArea();
+	    
+	    System.out.println(area2);
+	    
+	    Quadrado q = new Quadrado(5);
+	    double areaQuadrado = q.calcularQuadrado();
+	    System.out.println(areaQuadrado);
+	}
+
+}
