947f8e9da83d90adda14282e141ecc5cd1ed5977
bertolo321 <andre_bertolo@outlook.com>
2021-02-05 22:41:20 -0300

    Exemplo do  tipo de dado record.

--- src/o_tipo_de_dado_record/Quadrado.java
+++ src/o_tipo_de_dado_record/Quadrado.java
@@ -1,5 +1,18 @@
 package o_tipo_de_dado_record;
 
 public class Quadrado {
+	
+	private double lado;
+	
+	
+	
+	public Quadrado(double lado) {
+		this.lado = lado;
+	}
 
+
+
+	public double calcularQuadrado() {
+		return lado * lado;
+	}
 }
--- src/o_tipo_de_dado_record/Retangulo.java
+++ src/o_tipo_de_dado_record/Retangulo.java
@@ -1,5 +1,23 @@
 package o_tipo_de_dado_record;
 
 public class Retangulo {
-
+	private double largura;
+	private double altura;
+	
+	//construtor já definido
+	public Retangulo() {
+		this(4,3);
+		//System.out.println("Construtor ()");
+		//largura = 4;
+		//altura = 3;
+	}
+	
+	//construtor para inserir dados
+	public Retangulo(double largura,double altura) {
+		this.largura = largura;
+		this.altura = altura;
+	}
+	public double calcularArea() {
+		return largura * altura;
+	}
 }
